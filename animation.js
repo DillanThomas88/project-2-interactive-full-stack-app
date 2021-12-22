a39cc3729d059942511b648d5621701e23a76a4e
DillanThomas <mansor.dillan@gmail.com>
2021-12-21 22:42:28 -0800

    added spinner svgs and rotator scripts

--- animation.js
+++ animation.js
@@ -1,14 +1,32 @@
 let spinner = document.getElementById('spinner')
-let deg = 0
+let spinner2 = document.getElementById('spinner2')
+let wheel1 = document.getElementById('wheel1')
+let wheel2 = document.getElementById('wheel2')
+let wheel3 = document.getElementById('wheel3')
+let num = 0
+let num2 = 0
 spinner.style = "fill: #000;"
 
-spinnerRotate = () => {
+rotateSpinner = () => {
     setInterval(function(){
-        deg += 30
-        if(deg === 360){deg = 0}
-        spinner.style.transform = `rotate(${deg}deg)`
+        num += 30
+        if(num === 360){num = 0}
+        spinner.style.transform = `rotate(${num}deg)`
+        spinner2.style.transform = `rotate(${num}deg)`
     },100)
     
 }
 
-spinnerRotate()
+rotateWheel = () => {
+    setInterval(function(){
+        num2 += 2
+        if(num2 === 360){num2 = 0}
+        wheel1.style.transform = `rotate(${num2}deg)`
+        wheel2.style.transform = `rotate(${num2}deg)`
+        wheel3.style.transform = `rotate(${num2}deg)`
+    },5)
+    
+}
+
+rotateSpinner()
+rotateWheel()
