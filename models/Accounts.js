cb675812aeed3944d04e9b9a769897babde1a8b7
DillanThomas <mansor.dillan@gmail.com>
2022-01-03 18:49:06 -0800

    fixed updatehook

--- models/Accounts.js
+++ models/Accounts.js
@@ -25,12 +25,16 @@ Accounts.init(
     },
   },
   {
-    // hooks: {
-    //   beforeCreate: async (newUserData) => {
-    //     newUserData.password = await bcrypt.hash(newUserData.password, 10);
-    //     return newUserData;
-    //   },
-    // },
+    hooks: {
+      beforeCreate: async (newUserData) => {
+        newUserData.password = await bcrypt.hash(newUserData.password, 10);
+        return newUserData;
+      },
+      beforeUpdate: async (newUserData) => {
+        newUserData.newPassword = await bcrypt.hash(newUserData.newPassword, 10);
+        return newUserData;
+      },
+    },
     sequelize,
     timestamps: false,
     freezeTableName: true,
