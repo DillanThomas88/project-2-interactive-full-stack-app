63d4fbe00cbfa0b8ac5b0936739c62cfa17af954
Hayley McHugh <hayleym522@gmail.com>
2022-01-05 16:20:12 -0800

    .

--- models/index.js
+++ models/index.js
@@ -1,24 +1,7 @@
 const User = require('./User');
-const Accounts = require('./Accounts');
-const Cards = require('./Cards');
 const Bills = require('./Bills');
-const Debt = require('./Debt');
 
 
-User.hasMany(Accounts, {
-    foreignkey: "user_id",
-    onDelete: "CASCADE",
-})
-Accounts.belongsTo(User, {
-    foreignKey: "user_id",
-})
-User.hasMany(Cards, {
-    foreignkey: "user_id",
-    onDelete: "CASCADE",
-})
-Cards.belongsTo(User, {
-    foreignKey: "user_id",
-})
 User.hasMany(Bills, {
     foreignkey: "user_id",
     onDelete: "CASCADE",
@@ -26,13 +9,6 @@ User.hasMany(Bills, {
 Bills.belongsTo(User, {
     foreignKey: "user_id",
 })
-User.hasMany(Debt, {
-    foreignkey: "user_id",
-    onDelete: "CASCADE",
-})
-Debt.belongsTo(User, {
-    foreignKey: "user_id",
-})
 
 
-module.exports = { User, Accounts, Cards, Bills, Debt };
+module.exports = { User, Bills };
