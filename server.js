c1f26e218cb416e59f6a42f4d0be431c58dd7dc6
DillanThomas <62412765+DillanThomas88@users.noreply.github.com>
2022-02-16 20:16:03 -0800

    Update server.js

--- server.js
+++ server.js
@@ -13,7 +13,6 @@ const PORT = process.env.PORT || 3001;
 const hbs = exphbs.create({ helpers });
 
 const sess = {
-  secret: 'Super secret secret',
   cookie: {},
   resave: false,
   saveUninitialized: true,
